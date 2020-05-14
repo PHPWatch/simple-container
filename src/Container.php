126b08a5824c5f0dfa9fa0f49902473a689e344f
Ayesh Karunaratne <ayesh@ayesh.me>
2020-05-14 20:00:37 +0700

    Remove trailing dot in exception messages

--- src/Container.php
+++ src/Container.php
@@ -22,7 +22,7 @@ class Container implements ArrayAccess, ContainerInterface {
 
     private function getService(string $id) {
         if (!$this->has($id)) {
-            throw new NotFoundException(sprintf('Container key "%s" is not defined.', $id));
+            throw new NotFoundException(sprintf('Container key "%s" is not defined', $id));
         }
 
         if (!is_callable($this->definitions[$id]) || isset($this->protected[$id])) {
@@ -58,7 +58,7 @@ class Container implements ArrayAccess, ContainerInterface {
 
     public function setFactory(string $id, ?callable $value = null): void {
         if ($value === null && !$this->has($id)) {
-            throw new BadMethodCallException(sprintf('Attempt to set container ID "%s" as factory, but it is not already set nor provided in the function call.', $id));
+            throw new BadMethodCallException(sprintf('Attempt to set container ID "%s" as factory, but it is not already set nor provided in the function call', $id));
         }
 
         $this->definitions[$id] = $value;
