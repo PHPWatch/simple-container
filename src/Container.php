c8e65408268c640bbf49ebc82909ef8875ae8e50
Ayesh Karunaratne <ayesh@aye.sh>
2021-09-24 14:38:01 +0530

    Add return types for PHP 8.1 compatibility

--- src/Container.php
+++ src/Container.php
@@ -6,7 +6,6 @@ use ArrayAccess;
 use PHPWatch\SimpleContainer\Exception\BadMethodCallException;
 use PHPWatch\SimpleContainer\Exception\NotFoundException;
 use Psr\Container\ContainerInterface;
-
 use function array_key_exists;
 use function is_callable;
 use function sprintf;
@@ -19,83 +18,6 @@ class Container implements ArrayAccess, ContainerInterface {
 
 	public function __construct(array $definitions = []) {
 		$this->definitions = $definitions;
-	}
-
-	public function setProtected(string $id, ?callable $value = null): void {
-		if ($value === null) {
-			$value = $this->getDefaultDefinition(
-				$id,
-				sprintf(
-					'Attempt to set container ID "%s" as protected, but it is not already set nor provided in the function call.',
-					$id
-				)
-			);
-		}
-
-		$this->set($id, $value);
-		$this->protected[$id] = true;
-	}
-
-	private function getDefaultDefinition(string $id, string $exception_message): callable {
-		if (!$this->has($id)) {
-			throw new BadMethodCallException($exception_message);
-		}
-		if (!is_callable($this->definitions[$id])) {
-			throw new BadMethodCallException(
-				sprintf(
-					'Definition for "%s" expected to be a callable, "%s" found',
-					$id,
-					\gettype($this->definitions[$id])
-				)
-			);
-		}
-
-		return $this->definitions[$id];
-	}
-
-	/**
-	 * @inheritDoc
-	 */
-	public function has(string $id): bool {
-		return array_key_exists($id, $this->definitions);
-	}
-
-	public function set(string $id, $value): void {
-		if (array_key_exists($id, $this->definitions)) {
-			unset($this->generated[$id], $this->factories[$id], $this->protected[$id]);
-		}
-		$this->definitions[$id] = $value;
-	}
-
-	public function setFactory(string $id, ?callable $value = null): void {
-		if ($value === null) {
-			$value = $this->getDefaultDefinition(
-				$id,
-				sprintf(
-					'Attempt to set container ID "%s" as factory, but it is not already set nor provided in the function call',
-					$id
-				)
-			);
-		}
-
-		$this->set($id, $value);
-		$this->factories[$id] = true;
-	}
-
-	public function offsetSet($id, $value): void {
-		$this->set($id, $value);
-	}
-
-	public function offsetUnset($id): void {
-		unset($this->definitions[$id], $this->generated[$id], $this->factories[$id], $this->protected[$id]);
-	}
-
-	public function offsetExists(mixed $id): bool {
-		return array_key_exists($id, $this->definitions);
-	}
-
-	public function offsetGet(mixed $id): mixed {
-		return $this->getService($id);
 	}
 
 	private function getService(string $id) {
@@ -118,10 +40,69 @@ class Container implements ArrayAccess, ContainerInterface {
 		return $this->generated[$id] = $this->definitions[$id]($this);
 	}
 
+	public function set(string $id, $value): void {
+		if (array_key_exists($id, $this->definitions)) {
+			unset($this->generated[$id], $this->factories[$id], $this->protected[$id]);
+		}
+		$this->definitions[$id] = $value;
+	}
+
+	public function setProtected(string $id, ?callable $value = null): void {
+		if ($value === null) {
+			$value = $this->getDefaultDefinition($id, sprintf('Attempt to set container ID "%s" as protected, but it is not already set nor provided in the function call.', $id));
+		}
+
+		$this->set($id, $value);
+		$this->protected[$id] = true;
+	}
+
+	public function setFactory(string $id, ?callable $value = null): void {
+		if ($value === null) {
+			$value = $this->getDefaultDefinition($id, sprintf('Attempt to set container ID "%s" as factory, but it is not already set nor provided in the function call', $id));
+		}
+
+		$this->set($id, $value);
+		$this->factories[$id] = true;
+	}
+
+	private function getDefaultDefinition(string $id, string $exception_message): callable {
+		if (!$this->has($id)) {
+			throw new BadMethodCallException($exception_message);
+		}
+		if (!is_callable($this->definitions[$id])) {
+			throw new BadMethodCallException(sprintf('Definition for "%s" expected to be a callable, "%s" found', $id, \gettype($this->definitions[$id])));
+		}
+
+		return $this->definitions[$id];
+	}
+
+	public function offsetSet($id, $value): void {
+		$this->set($id, $value);
+	}
+
+	public function offsetUnset($id): void {
+		unset($this->definitions[$id], $this->generated[$id], $this->factories[$id], $this->protected[$id]);
+	}
+
+	public function offsetExists(mixed $id): bool {
+		return array_key_exists($id, $this->definitions);
+	}
+
+	public function offsetGet(mixed $id): mixed {
+		return $this->getService($id);
+	}
+
 	/**
 	 * @inheritDoc
 	 */
 	public function get(string $id) {
 		return $this->getService($id);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function has(string $id): bool {
+		return array_key_exists($id, $this->definitions);
 	}
 }
